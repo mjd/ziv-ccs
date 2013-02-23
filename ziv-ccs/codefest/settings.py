d2b3ddfa13834e7cf7b092c76057e2332c44ded8
Daniel Abeshouse <dabeshouse@gmail.com>
2013-02-23 17:26:16 -0500

    Modeling

--- ziv-ccs/codefest/settings.py
+++ ziv-ccs/codefest/settings.py
@@ -124,7 +124,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
-    'codefest.civichero',
+    'bootstrap_toolkit',
+    'codefest.civichero'
 )
 
 # A sample logging configuration. The only tangible logging
