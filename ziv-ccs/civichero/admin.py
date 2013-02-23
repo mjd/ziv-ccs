1d51641337aa080509d2fbc11b783e6252183080
Jon Parise <jlparise@gmail.com>
2013-02-23 11:59:03 -0500

    Add some very basic models and admin.

--- ziv-ccs/civichero/admin.py
+++ ziv-ccs/civichero/admin.py
@@ -11,3 +11,11 @@ class UserAdmin(admin.ModelAdmin):
     model = models.User
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Profile)
+admin.site.register(models.Organization)
+admin.site.register(models.Organizer)
+admin.site.register(models.Achievable)
+admin.site.register(models.Achieved)
+admin.site.register(models.Activity)
+admin.site.register(models.Event)
+admin.site.register(models.Reward)
--- ziv-ccs/civichero/models.py
+++ ziv-ccs/civichero/models.py
@@ -3,8 +3,52 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
+class Profile(models.Model):
 
-    login = models.CharField(max_length = 25)
+    screen_name = models.CharField(max_length = 25)
+    email = models.EmailField(max_length = 50)
+    avatar = models.ImageField(upload_to='uploads')
+
+
+
+class User(Profile):
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
+    friends = models.ManyToManyField('self', symmetrical=True, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.last_name + ', ' + self.first_name
+
+class Organization(Profile):
+    pass
+
+
+class Organizer(models.Model):
+    pass
+
+
+class Achievable(models.Model):
+    """This defines how to earn an achievement.
+    """
+    pass
+
+
+class Achieved(models.Model):
+    """This is the achievement roll up for a user."""
+    pass
+
+
+class Activity(models.Model):
+    pass
+
+
+class Event(Activity):
+    pass
+
+
+class Reward(models.Model):
+    pass
+
+
+
+
