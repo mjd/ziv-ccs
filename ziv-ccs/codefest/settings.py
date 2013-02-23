abcab5014eb9781dd3c36bf428f9837afe9fa878
Jon Parise <jlparise@gmail.com>
2013-02-23 14:51:43 -0500

    Restructure project.

--- ziv-ccs/codefest/settings.py
+++ ziv-ccs/codefest/settings.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
-    'civichero',
+    'codefest.civichero',
 )
 
 # A sample logging configuration. The only tangible logging
--- ziv-ccs/codefest/urls.py
+++ ziv-ccs/codefest/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from django.views.generic import DetailView
+from civichero.views import CitizenDetailView
 
 
 admin.autodiscover()
@@ -19,12 +20,12 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
 
-     (r'^/*$', 'civichero.views.home'),
-     (r'^user/profile/(\w+)$', 'civichero.views.user_profile'),
-     (r'^organizer/(\w+)/dashboard/*$', 'civichero.views.organizer_dashboard'),
-     (r'^leaderboard/*$', 'civichero.views.leaderboard'),
-     (r'^checkin/*$', 'civichero.views.checkin'),
-     (r'^user/profile/(?P<screenname>[a-zA-Z0-9-]+)/$', CitizenDetailView.as_view(), name='user+profile'),
+     (r'^/*$', 'codefest.civichero.views.home'),
+     (r'^user/profile/(\w+)$', 'codefest.civichero.views.user_profile'),
+     (r'^organizer/(\w+)/dashboard/*$', 'codefest.civichero.views.organizer_dashboard'),
+     (r'^leaderboard/*$', 'codefest.civichero.views.leaderboard'),
+     (r'^checkin/*$', 'codefest.civichero.views.checkin'),
+     (r'^user/profile/(?P<screenname>[a-zA-Z0-9-]+)/$', CitizenDetailView.as_view()),
 
      #Auth
     #(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'myapp/login.html'}),
